8b37b647c77776fd0d1d1d7d3c8ba0d5c343121c
Thlurte <86393801+Thlurte@users.noreply.github.com>
2022-12-25 15:01:00 +0530

    new changes

--- .ipynb_checkpoints/preprocessor-checkpoint.py
+++ .ipynb_checkpoints/preprocessor-checkpoint.py
@@ -1,6 +1,14 @@
-# A function to extract dataset
+# 1) A function to extract dataset
 def unzip_rawdata(path):
     import zipfile
     rawdata = zipfile.ZipFile(path)
     rawdata.extractall()
     rawdata.close()
+    
+
+# 2) Function that gives a vague idea of the number of files and folders in a dataset
+def dir_explorer(path):
+    import os
+    for path,names,filenames in os.walk(path):
+        print(f'There are {len(names)} folderes and {len(filesnames)} files in {path}')
+        
