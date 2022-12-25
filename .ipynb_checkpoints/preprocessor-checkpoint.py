2f07853383e3f14443923ba4fc524cc1230e9f7e
Thlurte <86393801+Thlurte@users.noreply.github.com>
2022-12-25 18:16:39 +0530

    changes

--- .ipynb_checkpoints/preprocessor-checkpoint.py
+++ .ipynb_checkpoints/preprocessor-checkpoint.py
@@ -6,7 +6,7 @@ def unzip_rawdata(path):
     rawdata.close()
     
 
-# 2) Function that gives a vague idea of the number of files and folders in a dataset
+# 2) Function that gives a vague idea of what is in a dataset
 def dir_explorer(path):
     import os
     import matplotlib.pyplot
@@ -29,16 +29,23 @@ def dir_explorer(path):
     matplotlib.pyplot.pie([pie['train'],pie['test']],labels=pie.keys(),autopct='%.1f%%')
     matplotlib.pyplot.title('Train vs Test')
     matplotlib.pyplot.subplot(1,3,2)
-    matplotlib.pyplot.pie([value['train']],autopct=lambda p : '{:.2f}%  ({:,.0f})'.format(p,p * sum(value['train'])/100),labels=label['train'])
+    matplotlib.pyplot.pie([value['train']],autopct=lambda p : '{:.0f}%  ({:,.0f})'.format(p,p * sum(value['train'])/100),labels=label['train'])
     matplotlib.pyplot.title('Contents of training dataset')
     matplotlib.pyplot.subplot(1,3,3)
-    matplotlib.pyplot.pie([value['test']],autopct=lambda p : '{:.2f}%  ({:,.0f})'.format(p,p * sum(value['test'])/100),labels=label['test'])
+    matplotlib.pyplot.pie([value['test']],autopct=lambda p : '{:.0f}%  ({:,.0f})'.format(p,p * sum(value['test'])/100),labels=label['test'])
     matplotlib.pyplot.title('Contents of testing dataset')
     matplotlib.pyplot.show()
         
-# 3) Custom function i use for some weired reasons.
+# 3) Custom function i use for some unknown reasons.
 def giver():
     import re
     from datetime import datetime
     return re.sub('[:, ,.,-]',"",str(datetime.now()))
 
+# 4) Custom call-back function for tensorflow board
+def tensorboard_callback(dir_name,exp_name):
+    import tensorflow
+    log_dir = dir_name + "/" + exp_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(log_dir=log_dir)
+  print(f"Saving TensorBoard log files to: {log_dir}")
+  return tensorboard_callback
