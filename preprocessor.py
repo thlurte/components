e4d0b0189df393e20a3a3e8a4d06b9cbe43dffb8
Thlurte <86393801+Thlurte@users.noreply.github.com>
2022-12-25 15:56:53 +0530

    update

--- preprocessor.py
+++ preprocessor.py
@@ -9,8 +9,17 @@ def unzip_rawdata(path):
 # 2) Function that gives a vague idea of the number of files and folders in a dataset
 def dir_explorer(path):
     import os
+    import matplotlib.pyplot
+    a = {'train': 0
+        ,'test': 0}
     for path,names,filenames in os.walk(path):
         print(f'There are {len(names)} folderes and {len(filenames)} files in {path}')
+        if 'train' in path:
+            a['train'] += len(filenames)
+        else:
+            a['test'] += len(filenames)
+    return a
+    
         
         
 # 3) Custom function i use for some weired reasons.
